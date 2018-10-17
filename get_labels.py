ca33b75c75c75854c396a4724851c4a2e0496660
MapleYlq <1280906473@qq.com>
2018-10-17 09:25:24 +0800

    get_labels Final version
    get_labels Final version

--- get_labels.py
+++ get_labels.py
@@ -1,77 +1,25 @@
 # coding:utf-8
 '''
-@time:    Created on  2018-10-15 11:38:47
+@time:    Created on  2018-10-15 20:40:32
 @author:  Lanqing
-@Func:    src.rewrite_labels
+@Func:    本质上是利用dataframe 数据检索， 不要走偏了
 '''
-
-import pandas as pd
-from collections import OrderedDict
-from config import train_folder    
-
-file_ = train_folder + '/annotation/fformationGT.csv'
-final_groups, tmp_group = OrderedDict(), {}
-fid = open(file_, 'r')
-previous_time = 0
-
-for line in fid:
-    
-    if line:
-        
-        #### fetch line
-        line = line.strip('\n').split(',')
-        time_now = line[0]
-        data = line[1:]
-        
-        #### remove extra ','
-        ttmp = []
-        for item in data:
-            if item:
-                ttmp.append(int(item))
-        data = ttmp
-        
-        #### get group id
-        if 1 in data:
-            group_id = 1
-        elif 14 in data:
-            group_id = 2
-        elif 17 in data:
-            group_id = 3
-        elif 2 in data:
-            group_id = 4 
-        else:
-            group_id = 0
-            
-        #### fetch data
-        tmp_group[group_id] = data        
-        if  previous_time != time_now:
-            # print(time_now)
-            final_groups[time_now] = tmp_group
-            previous_time = time_now
-            print(time_now)
-            # print(tmp_group, final_groups[time_now])
-            print(final_groups[time_now])
-            
-# print(final_groups)
-for key in final_groups.keys():  # time
-    for per_group in final_groups[key].keys():  # group list
-        # print(final_groups[key][per_group])
-        pass
-        # print(i + 1, final_groups[key][per_group], (i + 1) in final_groups[key][per_group])
-
-
-#### Find the appear group of per person of all the time
-# person_dict, person_list = {}, []
-# for i in range(18):  # person
-#     per_person_dict = OrderedDict()
-#     for key in final_groups.keys():  # time
-#         for per_group in final_groups[key].keys():  # group list
-#             # print(key, per_group, final_groups[key], final_groups[key][per_group])
-#             # print(i + 1, final_groups[key][per_group], (i + 1) in final_groups[key][per_group])
-#             if (i + 1) in final_groups[key][per_group]:
-#                 # print(per_group)
-#                 per_person_dict[key] = per_group
-#                 person_list.append([key, per_group])
-#                 # print([key, per_group])
-#     df = pd.DataFrame.from_dict(per_person_dict, orient='index')
-#     df.to_csv('../data/tmp/' + str(i + 1) + '.csv')
+import pandas as pd, numpy as np
+##### Get data
+df = pd.read_csv('fformationGT.csv', skiprows=0)
+##### LineID to Time ID
+df['index'], df['GroupID'], new_df = df.index, np.NaN, pd.DataFrame()
+##### LineID to Group ID
+for i in range(len(df.iloc[:, 0])):
+    values = list(df.iloc[i, :][:10])
+    groupID = '1' if 1.0 in values else '2'  if 14.0 in values else '3' if 17.0 in  values else '4' if 2.0 in  values else '0'
+    df['GroupID'].iloc[i] = groupID
+##### User ID to Group ID
+for i in range(18):
+    new_df = pd.DataFrame()
+    loc = np.where(df.iloc[:, :12].values == ((i + 1) * 1.0))[0]  
+##### Finally Fetch data, save into file
+    new_df['index'] = df.iloc[loc, :]['index']
+    new_df['Time'] = df.iloc[loc, 0]
+    new_df['GroupID'] = df.iloc[loc, :]['GroupID']
+    new_df.to_csv('../data/tmp/' + str(i + 1) + '.csv')
